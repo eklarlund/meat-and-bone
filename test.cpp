6954d8c83bf4afc18aa4d407753a01f12f78a5aa
eklarlund <eklarlund@gmail.com>
2017-01-13 18:04:27 -0500

    clarification of test

--- test.cpp
+++ test.cpp
@@ -16,12 +16,12 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
-
+const double tolerance = .000001;
 bool equals(double a, double b) {
-	return a - b < .000001 && b - a < .000001;
+	return (a - b < tolerance) && (b - a < tolerance);
 }
 
-TEST_CASE("Factorials are computed", "[factorial]") {
+TEST_CASE("All", "[]") {
 	Eigen::MatrixXd V_work(4,3);
 	V_work << 0, 0, 0,
 		0, 1, 0,
@@ -51,30 +51,29 @@ TEST_CASE("Factorials are computed", "[factorial]") {
 	find_distance(V_work, N, V_ref, F_ref, distances, hits, misses);
 
 	for (int i = 0; i < distances.rows(); i++) {
-		REQUIRE(equals(distances(i), .6));
+		REQUIRE(equals(distances(i), .6));		//.6 is distance between surfaces
 	}
 
 	displace_vertices(V_work, N, distances, 0, 1, 0, 1);
 
 	for (int i = 0; i < distances.rows(); i++) {
-		REQUIRE(equals(V_work(i,2), .648));
+		REQUIRE(equals(V_work(i,2), .648));		//.648 is the value of smoothstep for x = .6
 	}
 
 	Eigen::MatrixXd N_smooth, V_smooth;
 	laplace_smooth(V_ref, F_ref, V_smooth, N_smooth, 1);
 
 	for (int i = 0; i < N.rows(); i++) {
-		REQUIRE(equals(N(i, 0), -1*N_smooth(i, 0)));
-		REQUIRE(equals(N(i, 1), -1*N_smooth(i, 1)));
-		REQUIRE(equals(N(i, 2), -1*N_smooth(i, 2)));
+		REQUIRE(equals(N(i, 0), -N_smooth(i, 0)));
+		REQUIRE(equals(N(i, 1), -N_smooth(i, 1)));
+		REQUIRE(equals(N(i, 2), -N_smooth(i, 2)));
 	}
 
 	Eigen::MatrixXd V_plus;
 	Eigen::MatrixXi F_plus;
 	Eigen::VectorXi borderLoop;
 
-	if (make_skirt(V_work, F_work, N, V_plus, F_plus, borderLoop, 3, 1, -1, 0))
-		cout << "Make skirt worked?";
+	REQUIRE(make_skirt(V_work, F_work, N, V_plus, F_plus, borderLoop, 3, 1, -1, 0));
 
 	REQUIRE(equals(V_plus(13, 2), V_work(0, 2) - 1));
 	REQUIRE(equals(V_plus(14, 2), V_work(1, 2) - 1));
@@ -82,8 +81,7 @@ TEST_CASE("Factorials are computed", "[factorial]") {
 
 	Eigen::MatrixXd V_out;
 	Eigen::MatrixXi F_out;
-	if (make_solid(V_plus, F_plus, borderLoop, V_out, F_out, 1, .5))
-		cout << "\nmake_solid worked?";
+	REQUIRE(make_solid(V_plus, F_plus, borderLoop, V_out, F_out, 1, .5));
 
 	REQUIRE(equals(V_out(16, 2), V_plus(0, 2)) -.5);
 }
